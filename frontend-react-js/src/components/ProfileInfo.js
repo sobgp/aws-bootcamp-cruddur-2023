890b58fb7c30b232f1378ce9ee2fc0aba7563850
sobgp gp <sobgp@outlook.com>
2023-04-17 22:00:34 +0000

    update

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -3,6 +3,9 @@ import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
 import React from "react";
 
 // [TODO] Authenication
+import { Auth } from 'aws-amplify';
+
+
 import Cookies from 'js-cookie'
 
 export default function ProfileInfo(props) {
@@ -13,15 +16,12 @@ export default function ProfileInfo(props) {
   }
 
   const signOut = async () => {
-    console.log('signOut')
-    // [TODO] Authenication
-    Cookies.remove('user.logged_in')
-    //Cookies.remove('user.name')
-    //Cookies.remove('user.username')
-    //Cookies.remove('user.email')
-    //Cookies.remove('user.password')
-    //Cookies.remove('user.confirmation_code')
-    window.location.href = "/"
+    try {
+        await Auth.signOut({ global: true });
+        window.location.href = "/"
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
   }
 
   const classes = () => {
